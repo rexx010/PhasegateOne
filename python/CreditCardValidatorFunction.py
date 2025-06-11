ac2acc7987a1d43c0460c60b9225954ee13870b1
rexx010 <grexx200@gmail.com>
2025-06-11 01:29:19 +0100

    updated

--- python/CreditCardValidatorFunction.py
+++ python/CreditCardValidatorFunction.py
@@ -1,201 +1,153 @@
 def validate(atm):
 
+	if(atm[0] < 3 or atm[0] > 6):
+		print("Credit Card Type: Invalid Card");
+		print("Credit Card Number: ", *atm);
+		print("Credit Card Digit Length: ", len(atm));
+		print("Credit Card Validity: Invalid");
+		print();
+		print();
 
-if(atm[0] < 3 || atm[0] > 6){
-console.log();
-console.log("Credit Card Type: Invalid Card");
+	elif(atm[0] == 4):
+		print("Credit Card Type: Visa card");
 
-let GodSaveMe = " ";
-for(let number = 0; number < atm.length; number++){
-GodSaveMe += atm[number];
-}
-console.log("Credit Card Number: "+GodSaveMe);
-console.log("Credit Card Digit Length: "+atm.length);
-console.log("Credit Card Validity: Invalid");
-console.log();
-console.log();
+		secondDigits = 0;
+		oddPlace = 0;
 
-}else if(atm[0] == 4){
-	console.log("Credit Card Type: Visa card");
+		for num in range(0, len(atm), 2):
+			checker = atm[num] * 2;
+			mod = 0;
+			div = 0;
+			if checker > 9:
+				mod = checker % 10;
+				div = checker // 10;
+				secondDigits += mod + div;
+			else:
+				secondDigits += checker;
+		
+		for num in range(1, len(atm), 2):
+			oddPlace += atm[num];
 
-	let secondDigits = 0;
-	let oddPlace = 0;
+		total = secondDigits + oddPlace;
 
-	for(let count = 0; count < atm.length; count += 2){
-		let checker = atm[count] * 2;
-		let mod = 0;
-		let div = 0;
-		if(checker > 9){
-			mod = checker % 10;
-			div = checker / 10;
-			secondDigits += Math.floor(mod + div);
-		}else{
-			secondDigits += checker;
-		}
-	}
+		if(total % 10 == 0):
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Valid");
+			print();
+			print();
+		else:
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Invalid");
+			print();
+			print();
+	
 
-	for(let counter = 1; counter < atm.length; counter += 2){
-		oddPlace += atm[counter];
-	}
-	let total = secondDigits + oddPlace;
-	if(total % 10 == 0){
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Valid");
-console.log();
-console.log();
-	}else{
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Invalid");
-console.log();
-console.log();
-	}
+	elif(atm[0] == 5):
+		print("Credit Card Type: Master card");
 
-}else if(atm[0] == 5){
-console.log("Credit Card Type: Master card");
+		secondDigits = 0;
+		oddPlace = 0;
 
-	let secondDigits = 0;
-	let oddPlace = 0;
-
-	for(let count = 0; count < atm.length; count += 2){
-		let checker = atm[count] * 2;
-		let mod = 0;
-		let div = 0;
-		if(checker > 9){
-			mod = checker % 10;
-			div = checker / 10;
-			secondDigits += Math.floor(mod + div);
-		}else{
-			secondDigits += checker;
-		}
-	}
-
-	for(let counter = 1; counter < atm.length; counter += 2){
-		oddPlace += atm[counter];
-	}
-	let total = secondDigits + oddPlace;
-
-	if(total % 10 == 0){
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Valid");
-console.log();
-console.log();
-	}else{
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Invalid");
-console.log();
-console.log();
-	}
+		for num in range(0, len(atm), 2):
+			checker = atm[num] * 2;
+			mod = 0;
+			div = 0;
+			if checker > 9:
+				mod = checker % 10;
+				div = checker // 10;
+				secondDigits += mod + div;
+			else:
+				secondDigits += checker;
+		
+		for num in range(1, len(atm), 2):
+			oddPlace += atm[num];
+	
+		total = secondDigits + oddPlace;
+		if(total % 10 == 0):
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Valid");
+			print();
+			print();
+		else:
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Invalid");
+			print();
+			print();
+	
 
 
-}else if(atm[0] == 6){
-console.log("Credit Card Type: Discover card");
+	elif(atm[0] == 6):
+		print("Credit Card Type: Discover card");
 
-	let secondDigits = 0;
-	let oddPlace = 0;
+		secondDigits = 0;
+		oddPlace = 0;
 
-	for(let count = 0; count < atm.length; count += 2){
-		let checker = atm[count] * 2;
-		let mod = 0;
-		let div = 0;
-		if(checker > 9){
-			mod = checker % 10;
-			div = checker / 10;
-			secondDigits += Math.floor(mod + div);
-		}else{
-			secondDigits += checker;
-		}
-	}
-
-	for(let counter = 1; counter < atm.length; counter += 2){
-		oddPlace += atm[counter];
-	}
-	let total = secondDigits + oddPlace;
-
-	if(total % 10 == 0){
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Valid");
-console.log();
-console.log();
-	}else{
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Invalid");
-console.log();
-console.log();
-	}
+		for num in range(0, len(atm), 2):
+			checker = atm[num] * 2;
+			mod = 0;
+			div = 0;
+			if checker > 9:
+				mod = checker % 10;
+				div = checker // 10;
+				secondDigits += mod + div;
+			else:
+				secondDigits += checker;
+		
+		for num in range(1, len(atm), 2):
+			oddPlace += atm[num];
+	
+		total = secondDigits + oddPlace;
+		if(total % 10 == 0):
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Valid");
+			print();
+			print();
+		else:
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Invalid");
+			print();
+			print();
+	
 
 
-}else if(atm[0] == 3 && atm[1] == 7){
-console.log("Credit Card Type: American Express card");
+	elif(atm[0] == 3 and atm[1] == 7):
+		print("Credit Card Type: American Express card");
 
-	let secondDigits = 0;
-	let oddPlace = 0;
+		secondDigits = 0;
+		oddPlace = 0;
 
-	for(let count = 0; count < atm.length; count += 2){
-		let checker = atm[count] * 2;
-		let mod = 0;
-		let div = 0;
-		if(checker > 9){
-			mod = checker % 10;
-			div = checker / 10;
-			secondDigits += Math.floor(mod + div);
-		}else{
-			secondDigits += checker;
-		}
-	}
-
-	for(let counter = 1; counter < atm.length; counter += 2){
-		oddPlace += atm[counter];
-	}
-	let total = secondDigits + oddPlace;
-
-	if(total % 10 == 0){
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Valid");
-console.log();
-console.log();
-	}else{
-		let GodSaveMe = " "; 
-		for(let number = 0; number < atm.length; number++){
-		GodSaveMe += atm[number];
-		}
-	console.log("Credit Card Number: "+GodSaveMe);
-	console.log("Credit Card Digits: "+num1);
-	console.log("Credit Card Validity Status: Invalid");
-console.log();
-console.log();
-	}
+		for num in range(0, len(atm), 2):
+			checker = atm[num] * 2;
+			mod = 0;
+			div = 0;
+			if checker > 9:
+				mod = checker % 10;
+				div = checker // 10;
+				secondDigits += mod + div;
+			else:
+				secondDigits += checker;
+		
+		for num in range(1, len(atm), 2):
+			oddPlace += atm[num];
+	
+		total = secondDigits + oddPlace;
+		if(total % 10 == 0):
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Valid");
+			print();
+			print();
+		else:
+			print("Credit Card Number: ", *atm);
+			print("Credit Card Digits: ", len(atm));
+			print("Credit Card Validity Status: Invalid");
+			print();
+			print();
+			print();
+	
