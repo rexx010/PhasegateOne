79ade15d0503b52c074bdba84032eb4f8d9fdefd
rexx010 <grexx200@gmail.com>
2025-06-12 03:01:39 +0100

    created MBTI test app in java

---
+++ java/MBTIPersonalityTest.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MBTIPersonalityTest{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+String[][] = {
+{"A. expend energy, enjoy groups", "B. conserve energy, enjoy one-on-one"}
+{"A. interpret literally", "B. look for meaning and possibilities"}
+{"A. logical, thinking, questioning", "B. empathetic, feeling, accommodating"}
+{"A. organized, orderly", "B. flexible, adaptable"}
+{"A. more outgoing, think out loud", "B. more reserved, think to yourself"}
+{"A. }
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+{}
+};
+
+
+
+
+
+
+
+
+
+
+}
+}
