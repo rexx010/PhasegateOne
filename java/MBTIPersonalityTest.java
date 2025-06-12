cac201d4e9979aa2f77cfbe92416ad708161c212
rexx010 <grexx200@gmail.com>
2025-06-12 16:37:21 +0100

    updated

--- java/MBTIPersonalityTest.java
+++ java/MBTIPersonalityTest.java
@@ -8,28 +8,35 @@ public static void main(String[] args){
 
 Scanner input = new Scanner(System.in);
 
-String[][] = {
+String[][] questionsToAsk = {
 {"A. expend energy, enjoy groups", "B. conserve energy, enjoy one-on-one"}
 {"A. interpret literally", "B. look for meaning and possibilities"}
 {"A. logical, thinking, questioning", "B. empathetic, feeling, accommodating"}
 {"A. organized, orderly", "B. flexible, adaptable"}
 {"A. more outgoing, think out loud", "B. more reserved, think to yourself"}
-{"A. }
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
-{}
+{"A. practical, realistic, experiential", "B. imaginative, innovative, theoretical"}
+{"A. candid, straigth forward, frank", "B. tactful, kind, encouraging"}
+{"A. plan, schedule", "B. unplanned, sponteneous"}
+{"A. seek many task, public activities, interacting with others", "B. seek private, solitary activities with quiet to concentrate"}
+{"A. standard, usual, conventional", "B. different, novel, unique"}
+{"A. firm, tend to criticize, hold the line", "B. gentle, tend to appreciate, conciliate"}
+{"A. regulated, structured", "B. easy-going, live and let live"}
+{"A. external, communicative, express yourself", "B. internal, reticent, keep to yourself"}
+{"A. focus on here-and-now", "B. look to the future, global perspective, big picture"}
+{"A. tough-minded, just", "B. tender-hearted, merciful"}
+{"A. preparation, plan ahead", "B. go with the flow, adapt as you go"}
+{"A. active, initiate", "B. reflective, delibrate"}
+{"A. facts, things, what is", "B. ideas, dreams, what could be, philosophical"}
+{"A. matter of fact, issue-oriented", "B. sensitive, people-oriented, compassionate"}
+{"A. control, govern", "B. latitude, freedom"}
 };
+
+System.out.println("What is your name?");
+String userName = input.nextLine();
+
+for(int counter = 0; counter < questionsToAsk.length; counter++){
+System.out.println()
+}
 
 
 
