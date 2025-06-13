aa2ff01ebd912101b2534b1606b6b2f926b30eca
rexx010 <grexx200@gmail.com>
2025-06-13 05:22:33 +0100

    updated

--- javascript/MBTIPersonalityTest.js
+++ javascript/MBTIPersonalityTest.js
@@ -127,6 +127,8 @@ MBTIPersonalityTestFunction.judgingORperceptiveCounter(userAnswer);
 
 
 }
+
+
 console.log();
 console.log();
 console.log("Hello "+userName+" you selected:");
@@ -138,57 +140,57 @@ console.log();
 
 result = MBTIPersonalityTestFunction.overallResult();
 console.log();
-if(result === ("INTJ"))[
+if(result === "INTJ"){
 console.log(result+" \n\nINTJ (Architect) is a personality type with the Introverted, Intuitive, Thinking, and Judging traits. These thoughtful tacticians love perfecting the details of life, applying creativity and rationality to everything they do. Their inner world is often a private, complex one.");
-]
-else if(result === ("INTP"))[
+}
+else if(result === "INTP"){
 console.log(result+" \n\nINTP (Logician) is a personality type with the Introverted, Intuitive, Thinking, and Prospecting traits. These flexible thinkers enjoy taking an unconventional approach to many aspects of life. They often seek out unlikely paths, mixing willingness to experiment with personal creativity.");
-]
-else if(result === ("ENTJ"))[
+}
+else if(result === "ENTJ"){
 console.log(result+" \n\n\nENTJ (Commander) is a personality type with the Extraverted, Intuitive, Thinking, and Judging traits. They are decisive people who love momentum and accomplishment. They gather information to construct their creative visions but rarely hesitate for long before acting on them.");
-]
-else if(result === ("ENTP"))[
+}
+else if(result === "ENTP"){
 console.log(result+" \n\nENTP (Debater) is a personality type with the Extraverted, Intuitive, Thinking, and Prospecting traits. They tend to be bold and creative, deconstructing and rebuilding ideas with great mental agility. They pursue their goals vigorously despite any resistance they might encounter."); 
-]
-else if(result === ("INFJ"))[
+}
+else if(result === "INFJ"){
 console.log(result+" \n\nINFJ (Advocate) is a personality type with the Introverted, Intuitive, Feeling, and Judging traits. They tend to approach life with deep thoughtfulness and imagination. Their inner vision, personal values, and a quiet, principled version of humanism guide them in all things.");
-]
-else if(result === ("INFP"))[
+}
+else if(result === "INFP"){
 console.log(result+" \n\nINFP (Mediator) is a personality type with the Introverted, Intuitive, Feeling, and Prospecting traits. These rare personality types tend to be quiet, open-minded, and imaginative, and they apply a caring and creative approach to everything they do.");
-]
-else if(result === ("ENFJ"))[
+}
+else if(result === "ENFJ"){
 console.log(result+" \n\nENFJ (Protagonist) is a personality type with the Extraverted, Intuitive, Feeling, and Judging traits. These warm, forthright types love helping others, and they tend to have strong ideas and values. They back their perspective with the creative energy to achieve their goals.");
-]
-else if(result === ("ENFP"))[
+}
+else if(result === "ENFP"){
 console.log(result+" \n\nENFP (Campaigner) is a personality type with the Extraverted, Intuitive, Feeling, and Prospecting traits. These people tend to embrace big ideas and actions that reflect their sense of hope and goodwill toward others. Their vibrant energy can flow in many directions.");
-]
-else if(result === ("ISTJ"))[
+}
+else if(result === "ISTJ"){
 console.log(result+" \n\nISTJ (Logistician) is a personality type with the Introverted, Observant, Thinking, and Judging traits. These people tend to be reserved yet willful, with a rational outlook on life. They compose their actions carefully and carry them out with methodical purpose.");
-]
-else if(result === ("ISFJ"))[
+}
+else if(result === "ISFJ"){
 console.log(result+" \n\nISFJ (Defender) is a personality type with the Introverted, Observant, Feeling, and Judging traits. These people tend to be warm and unassuming in their own steady way. They’re efficient and responsible, giving careful attention to practical details in their daily lives.");
-]
-else if(result === ("ESTJ"))[
+}
+else if(result === "ESTJ"){
 console.log(result+" \n\nESTJ (Executive) is a personality type with the Extraverted, Observant, Thinking, and Judging traits. They possess great fortitude, emphatically following their own sensible judgment. They often serve as a stabilizing force among others, able to offer solid direction amid adversity.");
-]
-else if(result === ("ESFJ"))[
+}
+else if(result === "ESFJ"){
 console.log(result+" \n\nESFJ (Consul) is a personality type with the Extraverted, Observant, Feeling, and Judging traits. They are attentive and people-focused, and they enjoy taking part in their social community. Their achievements are guided by decisive values, and they willingly offer guidance to others.");
-]
-else if(result === ("ISTP"))[
+}
+else if(result === "ISTP"){
 console.log(result+" \n\nISTP (Virtuoso) is a personality type with the Introverted, Observant, Thinking, and Prospecting traits. They tend to have an individualistic mindset, pursuing goals without needing much external connection. They engage in life with inquisitiveness and personal skill, varying their approach as needed.");
-]
-else if(result === ("ISFP"))[
+}
+else if(result === "ISFP"){
 console.log(result+" \n\nISFP (Adventurer) is a personality type with the Introverted, Observant, Feeling, and Prospecting traits. They tend to have open minds, approaching life, new experiences, and people with grounded warmth. Their ability to stay in the moment helps them uncover exciting potentials.");
-]
-else if(result === ("ESTP"))[
+}
+else if(result === "ESTP"){
 console.log(result+" \n\nESTP (Entrepreneur) is a personality type with the Extraverted, Observant, Thinking, and Prospecting traits. They tend to be energetic and action-oriented, deftly navigating whatever is in front of them. They love uncovering life’s opportunities, whether socializing with others or in more personal pursuits.");
-]
-else if(result === ("ESFP"))[
+}
+else if(result === "ESFP"){
 console.log(result+" \n\nESFP (Entertainer) is a personality type with the Extraverted, Observant, Feeling, and Prospecting traits. These people love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. They can be very social, often encouraging others into shared activities.");
-]
+}
 console.log();
 
 
 
-]
-]
+}
+}
