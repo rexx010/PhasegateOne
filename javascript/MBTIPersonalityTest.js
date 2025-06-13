2e41c314fd3801b5ba7b482aff023461d3550513
rexx010 <grexx200@gmail.com>
2025-06-13 05:42:47 +0100

    updated

--- javascript/MBTIPersonalityTest.js
+++ javascript/MBTIPersonalityTest.js
@@ -1,47 +1,47 @@
 
-let extrovertVsintrovert = [];
-let int AE = 0;
-let int BI = 0;
-let sensingVsintuitive = [];
-let int AS = 0;
-let int BN = 0;
+let extrovertVsletrovert = [];
+let AE = 0;
+let BI = 0;
+let sensingVsletuitive = [];
+let AS = 0;
+let BN = 0;
 let thinkingVsfeeling = [];
-let int AT = 0;
-let int BF = 0;
+let AT = 0;
+let BF = 0;
 let judgingVsperceptive = [];
-let int AJ = 0;
-let int BP = 0;
+let AJ = 0;
+let BP = 0;
 let result = "";
 
 
-const extrovertORintrovert = function (choosed, number){
-if(number == 1) extrovertVsintrovert[0] = choosed;
-if(number == 5) extrovertVsintrovert[1] = choosed;
-if(number == 9) extrovertVsintrovert[2] = choosed;
-if(number == 13) extrovertVsintrovert[3] = choosed;
-if(number == 17) extrovertVsintrovert[4] = choosed;
+const extrovertORletrovert = function (choosed, number){
+if(number == 1) extrovertVsletrovert[0] = choosed;
+if(number == 5) extrovertVsletrovert[1] = choosed;
+if(number == 9) extrovertVsletrovert[2] = choosed;
+if(number == 13) extrovertVsletrovert[3] = choosed;
+if(number == 17) extrovertVsletrovert[4] = choosed;
 
-return extrovertVsintrovert;
+return extrovertVsletrovert;
 }
 
 
-const extrovertORintrovertCounter = function (answer){
+const extrovertORletrovertCounter = function (answer){
 if(answer === "A") AE++;
 if(answer === "B") BI++;
 }
 
-const sensingORintuitive = function (choosed, number){
-if(number == 2) sensingVsintuitive[0] = choosed;
-if(number == 6) sensingVsintuitive[1] = choosed;
-if(number == 10) sensingVsintuitive[2] = choosed;
-if(number == 14) sensingVsintuitive[3] = choosed;
-if(number == 18) sensingVsintuitive[4] = choosed;
+const sensingORletuitive = function (choosed, number){
+if(number == 2) sensingVsletuitive[0] = choosed;
+if(number == 6) sensingVsletuitive[1] = choosed;
+if(number == 10) sensingVsletuitive[2] = choosed;
+if(number == 14) sensingVsletuitive[3] = choosed;
+if(number == 18) sensingVsletuitive[4] = choosed;
 
-return sensingVsintuitive;
+return sensingVsletuitive;
 }
 
 
-const sensingORintuitiveCounter = function(answer){
+const sensingORletuitiveCounter = function(answer){
 if(answer === "A") AS++;
 if(answer === "B") BN++;
 }
@@ -80,43 +80,43 @@ if(answer === "A") AJ++;
 if(answer === "B") BP++;
 }
 
-const extrovertORintrovertAnswer = function (){
-for(int counter = 0; counter < extrovertVsintrovert.length; counter++){
-System.out.println(extrovertVsintrovert[counter]);
+const extrovertORletrovertAnswer = function (){
+for(let counter = 0; counter < extrovertVsletrovert.length; counter++){
+console.logln(extrovertVsletrovert[counter]);
 }
-System.out.println("Numbers of A selected: "+AE);
-System.out.println("Numbers of B selected: "+BI);
-System.out.println();
+console.logln("Numbers of A selected: "+AE);
+console.logln("Numbers of B selected: "+BI);
+console.logln();
 }
 
 
-const sensingORintuitiveAnswer = function (){
-for(int counter = 0; counter < sensingVsintuitive.length; counter++){
-System.out.println(sensingVsintuitive[counter]);
+const sensingORletuitiveAnswer = function (){
+for(let counter = 0; counter < sensingVsletuitive.length; counter++){
+console.logln(sensingVsletuitive[counter]);
 }
-System.out.println("Numbers of A selected: "+AS);
-System.out.println("Numbers of B selected: "+BN);
-System.out.println();
+console.logln("Numbers of A selected: "+AS);
+console.logln("Numbers of B selected: "+BN);
+console.logln();
 }
 
 
 const thinkingORfeelingAnswer = function (){
-for(int counter = 0; counter < thinkingVsfeeling.length; counter++){
-System.out.println(thinkingVsfeeling[counter]);
+for(let counter = 0; counter < thinkingVsfeeling.length; counter++){
+console.logln(thinkingVsfeeling[counter]);
 }
-System.out.println("Numbers of A selected: "+AT);
-System.out.println("Numbers of B selected: "+BF);
-System.out.println();
+console.logln("Numbers of A selected: "+AT);
+console.logln("Numbers of B selected: "+BF);
+console.logln();
 }
 
 
 const judgingORperceptiveAnswer = function(){
-for(int counter = 0; counter < judgingVsperceptive.length; counter++){
-System.out.println(judgingVsperceptive[counter]);
+for(let counter = 0; counter < judgingVsperceptive.length; counter++){
+console.logln(judgingVsperceptive[counter]);
 }
-System.out.println("Numbers of A selected: "+AJ);
-System.out.println("Numbers of B selected: "+BP);
-System.out.println();
+console.logln("Numbers of A selected: "+AJ);
+console.logln("Numbers of B selected: "+BP);
+console.logln();
 }
 
 const overallResult = function (){
@@ -149,7 +149,7 @@ return result;
 //public static void overallResultType(){
 
 
-}
+
 
 
 
@@ -171,18 +171,18 @@ const prompt = input();
 
 let questionsToAsk = [
 ["A. expend energy, enjoy groups", "B. conserve energy, enjoy one-on-one"],
-["A. interpret literally", "B. look for meaning and possibilities"],
+["A. leterpret literally", "B. look for meaning and possibilities"],
 ["A. logical, thinking, questioning", "B. empathetic, feeling, accommodating"],
 ["A. organized, orderly", "B. flexible, adaptable"],
 ["A. more outgoing, think out loud", "B. more reserved, think to yourself"],
 ["A. practical, realistic, experiential", "B. imaginative, innovative, theoretical"],
 ["A. candid, straigth forward, frank", "B. tactful, kind, encouraging"],
 ["A. plan, schedule", "B. unplanned, sponteneous"],
-["A. seek many task, public activities, interacting with others", "B. seek private, solitary activities with quiet to concentrate"],
+["A. seek many task, public activities, leteracting with others", "B. seek private, solitary activities with quiet to concentrate"],
 ["A. standard, usual, conventional", "B. different, novel, unique"],
 ["A. firm, tend to criticize, hold the line", "B. gentle, tend to appreciate, conciliate"],
 ["A. regulated, structured", "B. easy-going, live and let live"],
-["A. external, communicative, express yourself", "B. internal, reticent, keep to yourself"],
+["A. external, communicative, express yourself", "B. leternal, reticent, keep to yourself"],
 ["A. focus on here-and-now", "B. look to the future, global perspective, big picture"],
 ["A. tough-minded, just", "B. tender-hearted, merciful"],
 ["A. preparation, plan ahead", "B. go with the flow, adapt as you go"],
@@ -208,16 +208,16 @@ userAnswer = userAnswer.toUpperCase();
 if(userAnswer === "A" || userAnswer == "B"){
 break;
 }else{
-System.out.print("Invalid choice... Try again");
+console.log("Invalid choice... Try again");
 }
 }
 
 if(userAnswer === "A"){
-MBTIPersonalityTestFunction.extrovertORintrovert(questionsToAsk[count][0], count+1);
+extrovertORletrovert(questionsToAsk[count][0], count+1);
 }else if(userAnswer === "B"){
-MBTIPersonalityTestFunction.extrovertORintrovert(questionsToAsk[count][1], count+1);
+extrovertORletrovert(questionsToAsk[count][1], count+1);
 }
-MBTIPersonalityTestFunction.extrovertORintrovertCounter(userAnswer);
+extrovertORletrovertCounter(userAnswer);
 }
 
 
@@ -231,16 +231,16 @@ userAnswer = userAnswer.toUpperCase();
 if(userAnswer === "A" || userAnswer === "B"){
 break;
 }else{
-System.out.print("Invalid choice... Try again");
+console.log("Invalid choice... Try again");
 }
 }
 
 if(userAnswer === "A"){
-MBTIPersonalityTestFunction.sensingORintuitive(questionsToAsk[count][0], count+1);
+sensingORletuitive(questionsToAsk[count][0], count+1);
 }else if(userAnswer === "B"){
-MBTIPersonalityTestFunction.sensingORintuitive(questionsToAsk[count][1], count+1);
+sensingORletuitive(questionsToAsk[count][1], count+1);
 }
-MBTIPersonalityTestFunction.sensingORintuitiveCounter(userAnswer);
+sensingORletuitiveCounter(userAnswer);
 }
 
 
@@ -254,16 +254,16 @@ userAnswer = userAnswer.toUpperCase();
 if(userAnswer === "A" || userAnswer === "B"){
 break;
 }else{
-System.out.print("Invalid choice... Try again");
+console.log("Invalid choice... Try again");
 }
 }
 
 if(userAnswer === "A"){
-MBTIPersonalityTestFunction.thinkingORfeeling(questionsToAsk[count][0], count+1);
+thinkingORfeeling(questionsToAsk[count][0], count+1);
 }else if(userAnswer === "B"){
-MBTIPersonalityTestFunction.thinkingORfeeling(questionsToAsk[count][1], count+1);
+thinkingORfeeling(questionsToAsk[count][1], count+1);
 }
-MBTIPersonalityTestFunction.thinkingORfeelingCounter(userAnswer);
+thinkingORfeelingCounter(userAnswer);
 }
 
 
@@ -278,16 +278,16 @@ userAnswer = userAnswer.toUpperCase();
 if(userAnswer === "A" || userAnswer === "B"){
 break;
 }else{
-System.out.print("Invalid choice... Try again");
+console.log("Invalid choice... Try again");
 }
 }
 
 if(userAnswer === "A"){
-MBTIPersonalityTestFunction.judgingORperceptive(questionsToAsk[count][0], count+1);
+judgingORperceptive(questionsToAsk[count][0], count+1);
 }else if(userAnswer === "B"){
-MBTIPersonalityTestFunction.judgingORperceptive(questionsToAsk[count][1], count+1);
+judgingORperceptive(questionsToAsk[count][1], count+1);
 }
-MBTIPersonalityTestFunction.judgingORperceptiveCounter(userAnswer);
+judgingORperceptiveCounter(userAnswer);
 }
 
 
@@ -297,43 +297,43 @@ MBTIPersonalityTestFunction.judgingORperceptiveCounter(userAnswer);
 console.log();
 console.log();
 console.log("Hello "+userName+" you selected:");
-MBTIPersonalityTestFunction.extrovertORintrovertAnswer();
-MBTIPersonalityTestFunction.sensingORintuitiveAnswer();
-MBTIPersonalityTestFunction.thinkingORfeelingAnswer();
-MBTIPersonalityTestFunction.judgingORperceptiveAnswer();
+extrovertORletrovertAnswer();
+sensingORletuitiveAnswer();
+thinkingORfeelingAnswer();
+judgingORperceptiveAnswer();
 console.log();
 
-result = MBTIPersonalityTestFunction.overallResult();
+result = overallResult();
 console.log();
-if(result === "INTJ"){
-console.log(result+" \n\nINTJ (Architect) is a personality type with the Introverted, Intuitive, Thinking, and Judging traits. These thoughtful tacticians love perfecting the details of life, applying creativity and rationality to everything they do. Their inner world is often a private, complex one.");
+if(result === "letJ"){
+console.log(result+" \n\nletJ (Architect) is a personality type with the letroverted, letuitive, Thinking, and Judging traits. These thoughtful tacticians love perfecting the details of life, applying creativity and rationality to everything they do. Their inner world is often a private, complex one.");
 }
-else if(result === "INTP"){
-console.log(result+" \n\nINTP (Logician) is a personality type with the Introverted, Intuitive, Thinking, and Prospecting traits. These flexible thinkers enjoy taking an unconventional approach to many aspects of life. They often seek out unlikely paths, mixing willingness to experiment with personal creativity.");
+else if(result === "letP"){
+console.log(result+" \n\nletP (Logician) is a personality type with the letroverted, letuitive, Thinking, and Prospecting traits. These flexible thinkers enjoy taking an unconventional approach to many aspects of life. They often seek out unlikely paths, mixing willingness to experiment with personal creativity.");
 }
 else if(result === "ENTJ"){
-console.log(result+" \n\n\nENTJ (Commander) is a personality type with the Extraverted, Intuitive, Thinking, and Judging traits. They are decisive people who love momentum and accomplishment. They gather information to construct their creative visions but rarely hesitate for long before acting on them.");
+console.log(result+" \n\n\nENTJ (Commander) is a personality type with the Extraverted, letuitive, Thinking, and Judging traits. They are decisive people who love momentum and accomplishment. They gather information to construct their creative visions but rarely hesitate for long before acting on them.");
 }
 else if(result === "ENTP"){
-console.log(result+" \n\nENTP (Debater) is a personality type with the Extraverted, Intuitive, Thinking, and Prospecting traits. They tend to be bold and creative, deconstructing and rebuilding ideas with great mental agility. They pursue their goals vigorously despite any resistance they might encounter."); 
+console.log(result+" \n\nENTP (Debater) is a personality type with the Extraverted, letuitive, Thinking, and Prospecting traits. They tend to be bold and creative, deconstructing and rebuilding ideas with great mental agility. They pursue their goals vigorously despite any resistance they might encounter."); 
 }
 else if(result === "INFJ"){
-console.log(result+" \n\nINFJ (Advocate) is a personality type with the Introverted, Intuitive, Feeling, and Judging traits. They tend to approach life with deep thoughtfulness and imagination. Their inner vision, personal values, and a quiet, principled version of humanism guide them in all things.");
+console.log(result+" \n\nINFJ (Advocate) is a personality type with the letroverted, letuitive, Feeling, and Judging traits. They tend to approach life with deep thoughtfulness and imagination. Their inner vision, personal values, and a quiet, principled version of humanism guide them in all things.");
 }
 else if(result === "INFP"){
-console.log(result+" \n\nINFP (Mediator) is a personality type with the Introverted, Intuitive, Feeling, and Prospecting traits. These rare personality types tend to be quiet, open-minded, and imaginative, and they apply a caring and creative approach to everything they do.");
+console.log(result+" \n\nINFP (Mediator) is a personality type with the letroverted, letuitive, Feeling, and Prospecting traits. These rare personality types tend to be quiet, open-minded, and imaginative, and they apply a caring and creative approach to everything they do.");
 }
 else if(result === "ENFJ"){
-console.log(result+" \n\nENFJ (Protagonist) is a personality type with the Extraverted, Intuitive, Feeling, and Judging traits. These warm, forthright types love helping others, and they tend to have strong ideas and values. They back their perspective with the creative energy to achieve their goals.");
+console.log(result+" \n\nENFJ (Protagonist) is a personality type with the Extraverted, letuitive, Feeling, and Judging traits. These warm, forthright types love helping others, and they tend to have strong ideas and values. They back their perspective with the creative energy to achieve their goals.");
 }
 else if(result === "ENFP"){
-console.log(result+" \n\nENFP (Campaigner) is a personality type with the Extraverted, Intuitive, Feeling, and Prospecting traits. These people tend to embrace big ideas and actions that reflect their sense of hope and goodwill toward others. Their vibrant energy can flow in many directions.");
+console.log(result+" \n\nENFP (Campaigner) is a personality type with the Extraverted, letuitive, Feeling, and Prospecting traits. These people tend to embrace big ideas and actions that reflect their sense of hope and goodwill toward others. Their vibrant energy can flow in many directions.");
 }
 else if(result === "ISTJ"){
-console.log(result+" \n\nISTJ (Logistician) is a personality type with the Introverted, Observant, Thinking, and Judging traits. These people tend to be reserved yet willful, with a rational outlook on life. They compose their actions carefully and carry them out with methodical purpose.");
+console.log(result+" \n\nISTJ (Logistician) is a personality type with the letroverted, Observant, Thinking, and Judging traits. These people tend to be reserved yet willful, with a rational outlook on life. They compose their actions carefully and carry them out with methodical purpose.");
 }
 else if(result === "ISFJ"){
-console.log(result+" \n\nISFJ (Defender) is a personality type with the Introverted, Observant, Feeling, and Judging traits. These people tend to be warm and unassuming in their own steady way. They’re efficient and responsible, giving careful attention to practical details in their daily lives.");
+console.log(result+" \n\nISFJ (Defender) is a personality type with the letroverted, Observant, Feeling, and Judging traits. These people tend to be warm and unassuming in their own steady way. They’re efficient and responsible, giving careful attention to practical details in their daily lives.");
 }
 else if(result === "ESTJ"){
 console.log(result+" \n\nESTJ (Executive) is a personality type with the Extraverted, Observant, Thinking, and Judging traits. They possess great fortitude, emphatically following their own sensible judgment. They often serve as a stabilizing force among others, able to offer solid direction amid adversity.");
@@ -342,20 +342,19 @@ else if(result === "ESFJ"){
 console.log(result+" \n\nESFJ (Consul) is a personality type with the Extraverted, Observant, Feeling, and Judging traits. They are attentive and people-focused, and they enjoy taking part in their social community. Their achievements are guided by decisive values, and they willingly offer guidance to others.");
 }
 else if(result === "ISTP"){
-console.log(result+" \n\nISTP (Virtuoso) is a personality type with the Introverted, Observant, Thinking, and Prospecting traits. They tend to have an individualistic mindset, pursuing goals without needing much external connection. They engage in life with inquisitiveness and personal skill, varying their approach as needed.");
+console.log(result+" \n\nISTP (Virtuoso) is a personality type with the letroverted, Observant, Thinking, and Prospecting traits. They tend to have an individualistic mindset, pursuing goals without needing much external connection. They engage in life with inquisitiveness and personal skill, varying their approach as needed.");
 }
 else if(result === "ISFP"){
-console.log(result+" \n\nISFP (Adventurer) is a personality type with the Introverted, Observant, Feeling, and Prospecting traits. They tend to have open minds, approaching life, new experiences, and people with grounded warmth. Their ability to stay in the moment helps them uncover exciting potentials.");
+console.log(result+" \n\nISFP (Adventurer) is a personality type with the letroverted, Observant, Feeling, and Prospecting traits. They tend to have open minds, approaching life, new experiences, and people with grounded warmth. Their ability to stay in the moment helps them uncover exciting potentials.");
 }
 else if(result === "ESTP"){
 console.log(result+" \n\nESTP (Entrepreneur) is a personality type with the Extraverted, Observant, Thinking, and Prospecting traits. They tend to be energetic and action-oriented, deftly navigating whatever is in front of them. They love uncovering life’s opportunities, whether socializing with others or in more personal pursuits.");
 }
 else if(result === "ESFP"){
-console.log(result+" \n\nESFP (Entertainer) is a personality type with the Extraverted, Observant, Feeling, and Prospecting traits. These people love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. They can be very social, often encouraging others into shared activities.");
+console.log(result+" \n\nESFP (Entertainer) is a personality type with the Extraverted, Observant, Feeling, and Prospecting traits. These people love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. They can be very social, often encouraging others leto shared activities.");
 }
 console.log();
 
 
 
-}
-}
+
