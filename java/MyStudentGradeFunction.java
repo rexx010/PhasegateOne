8b294baaee99e0a4e39d9332251860dac95768a6
rexx010 <grexx200@gmail.com>
2025-06-14 06:09:35 +0100

    created a student grade app function in java

---
+++ java/MyStudentGradeFunction.java
@@ -0,0 +1,28 @@
+public class MyStudentGradeFunction{
+
+static int student = 0;
+static int subject = 0;
+static int[][] studentAndSbuject = new int[student][subject];
+
+public static int students(int number){
+student = number;
+return student;
+}
+
+public static int subjects(int number){
+subject = number;
+return subject;
+}
+
+public static int subjects(int number){
+subject = number;
+return subject;
+}
+
+
+
+
+
+
+
+}
