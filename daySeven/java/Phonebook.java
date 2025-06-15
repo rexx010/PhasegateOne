33a5bdb01a9f65fa041c5aa684d38d2499144bff
rexx010 <grexx200@gmail.com>
2025-06-15 06:24:12 +0100

    created  a phonebook app in java

---
+++ daySeven/java/Phonebook.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Phonebook{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("What is your name?");
+String userName = input.nextLine();
+System.out.println();
+
+String message = """
+press 1 to Add contact.
+press 2 to View contact.
+press 3 to Remove contact.
+press 4 to Find contact by phone number.
+press 5 to Find contact by first name.
+press 6 to Find contact by last name.
+press 7 to Edit contact.
+press 0 to Exist Phonebook.
+""";
+boolean myCondition = true;
+while(myCondition){
+System.out.println("Welcome to your phonebook app MR. "+userName);
+System.out.println(message);
+System.out.println();
+String userChoice = input.nextLine();
+switch(userChoice){
+case "1":
+	System.out.println("To add contact...");
+	System.out.println("Enter the first name...");
+	String firstName = input.nextLine();
+	System.out.println("Enter the second name...");
+	String secondtName = input.nextLine();
+	System.out.println("Enter the phone number...");
+	String phoneNumber = input.nextLine();
+	System.out.println();
+	System.out.println("Contact saved...");
+	System.out.println(firstName+" "+secondtName+" : "+phoneNumber);
+	System.out.println();
+	PhonebookFunction.addContact(firstName, secondtName, phoneNumber);
+	break;
+case "2":
+	PhonebookFunction.viewContact();
+	System.out.println();
+	break;
+case "3":
+	break;
+case "4":
+	break;
+case "5":
+	break;
+case "6":
+	break;
+case "7":
+	break;
+case "0":
+	myCondition = false;
+	System.out.println("Goodbye...");
+	break;
+default:
+	System.out.println("Invalid input... Try again");
+	break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
+}
